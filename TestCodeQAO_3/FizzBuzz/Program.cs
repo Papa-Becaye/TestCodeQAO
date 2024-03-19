5e6763839e9c5f021c6d4af7ccf4efd99859f64b
Papa Bécaye FALL <kountabecaye009@gmail.com>
2024-03-19 22:59:03 +0000

    Update FizzBuzz

---
+++ TestCodeQAO_3/FizzBuzz/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace FizzBuzz
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            static List<string> fizzBuzz(int n)
+            {
+                List<string> tabResultNumber = [];
+                int i = 1;
+               
+                while (i <= n)
+                {
+                    if (i % 3 == 0 && i % 5 == 0)
+                    {
+                        tabResultNumber.Add("FizzBuzz");
+                    }
+                    else if (i % 3 == 0)
+                    {
+                        tabResultNumber.Add("Fizz");
+                    }
+                    else if (i % 5 == 0)
+                    {
+                        tabResultNumber.Add("Buzz");
+                    }
+                    else
+                    {
+                        tabResultNumber.Add(Convert.ToString(i));
+                    }
+                    i++;
+                }
+                return tabResultNumber;
+            }
+            List<string> strings = new List<string>();
+            Console.WriteLine("Veuillez saisir un nombre entier :");
+            string? val = Console.ReadLine();
+
+            strings = fizzBuzz(Convert.ToInt32(val));
+
+            Console.WriteLine("Nmbre :"+strings.Count);
+            for (int i = 0; i < strings.Count; i++)
+            {
+                Console.WriteLine(strings[i]);
+            }
+        }
+    }
+}
